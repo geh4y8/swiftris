257a6d390979646490c9472f76d63bbcfd80e167
Guy Halperin <guyhalperin@Guys-MacBook-Air.local>
2014-11-05 19:00:52 +0000

    add ticker

--- Array2D.swift
+++ Array2D.swift
@@ -1,9 +1,27 @@
-//
-//  Array2D.swift
-//  Swiftris
-//
-//  Created by Guy Halperin on 11/5/14.
-//  Copyright (c) 2014 Guy Halperin. All rights reserved.
-//
 
-import Foundation
+// #1
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+
+// #2
+    var array: Array<T?>
+
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+// #3
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+
+// #4
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
